b43273c913ba49604684db04bad2b44a6ee24089
Josh Lavallee <44557318+jlavallee76@users.noreply.github.com>
2020-06-14 18:28:00 -0500

    commit on master

---
+++ portfolioscrypt.js
@@ -0,0 +1,11 @@
+/*************************************************
+- Name: Josh Lavallee                            *
+- Date Created: 06/14/2020                       *
+- Date Modified: 06/14/2020                      *
+- Description: Main JS Script                    *
+*************************************************/
+
+function start()
+{
+
+}
